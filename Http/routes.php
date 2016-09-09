fc9164763a978dd6eb5c8236e23a63ed57fd85ae
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-09-09 14:40:01 +0300

    Move resetter to user module

--- Http/routes.php
+++ Http/routes.php
@@ -1,5 +1,2 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Ignite\Core\Http\Controllers'], function() {
-    Route::get('/', 'AdminBaseController@index');
-});
