1f1a0d99c65c1367fe9d68a6875f2096f60272c9
Samuel Dervis <samueldervis@gmail.com>
2017-02-17 23:55:08 +0300

    Update phpDoc

--- Entities/DashboardWidgets.php
+++ Entities/DashboardWidgets.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Ignite\Core\Entities\DashboardWidgets
  *
- * @property integer $id
- * @property integer $user_id
+ * @property int $id
+ * @property int $user_id
  * @property string $widgets
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereCreatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereUpdatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereUserId($value)
  * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereWidgets($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Ignite\Core\Entities\DashboardWidgets whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class DashboardWidgets extends Model {
