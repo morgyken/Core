2e10eb2c04144be265fadba7482e0877689dc0cb
Samuel Dervis <samueldervis@gmail.com>
2017-08-31 10:24:47 +0300

    update base controller

--- Http/Controllers/AdminBaseController.php
+++ Http/Controllers/AdminBaseController.php
@@ -13,11 +13,12 @@
 namespace Ignite\Core\Http\Controllers;
 
 use FloatingPoint\Stylist\Facades\ThemeFacade as Theme;
-use Illuminate\Routing\Controller;
 use Ignite\Core\Foundation\Asset\Manager\AssetManager;
 use Ignite\Core\Foundation\Asset\Pipeline\AssetPipeline;
+use Illuminate\Routing\Controller;
 
-class AdminBaseController extends Controller {
+class AdminBaseController extends Controller
+{
 
     /**
      * @var AssetManager
@@ -34,21 +35,26 @@ class AdminBaseController extends Controller {
      * @var array
      */
     public $data = [];
+    /** @var  array */
+    public $input;
 
     /**
      * @var AssetPipeline
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->assetManager = app(AssetManager::class);
         $this->assetPipeline = app(AssetPipeline::class);
         $this->addAssets();
         $this->requireDefaultAssets();
+        $this->search();
     }
 
     /**
      * Add the assets from the config file on the asset manager
      */
-    private function addAssets() {
+    private function addAssets()
+    {
         $real_assets = mconfig('core.core.admin-assets');
         foreach ($real_assets as $assetName => $path) {
             if (key($path) == 'theme') {
@@ -62,9 +68,21 @@ class AdminBaseController extends Controller {
     /**
      * Require the default assets from config file on the asset pipeline
      */
-    private function requireDefaultAssets() {
+    private function requireDefaultAssets()
+    {
         $this->assetPipeline->requireCss(mconfig('core.core.admin-required-assets.css'));
         $this->assetPipeline->requireJs(mconfig('core.core.admin-required-assets.js'));
     }
 
+    /**
+     * @return array
+     */
+    private function search()
+    {
+        $this->input = \request()->all();
+        unset($this->input['_token']);
+        if (empty($this->input['id'])) {
+            unset($this->input['id']);
+        }
+    }
 }
