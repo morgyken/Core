20ae49c483094e032a43620f1c93a2b3baa66125
Samuel Dervis <samueldervis@gmail.com>
2017-11-01 15:06:31 +0300

    NEW SEED OPTIONS

--- Console/Installers/Scripts/ModuleSeeders.php
+++ Console/Installers/Scripts/ModuleSeeders.php
@@ -15,13 +15,16 @@ namespace Ignite\Core\Console\Installers\Scripts;
 use Illuminate\Console\Command;
 use Ignite\Core\Console\Installers\SetupScript;
 
-class ModuleSeeders implements SetupScript {
+class ModuleSeeders implements SetupScript
+{
 
     /**
      * @var array
      */
     protected $modules = [
         'Core',
+        'Users',
+        'Setup',
     ];
     protected $secondary = [
         'Setup',
@@ -31,6 +34,8 @@ class ModuleSeeders implements SetupScript {
         'Messaging',
         'Finance',
         'Inventory',
+        'Setup',
+        'Core',
         'Users',
         'Settings',
     ];
@@ -40,25 +45,34 @@ class ModuleSeeders implements SetupScript {
      * @param  Command $command
      * @return mixed
      */
-    public function fire(Command $command) {
+    public function fire(Command $command)
+    {
         $command->blockMessage('Seeds', 'Running the module seeds ...', 'comment');
-        foreach (\Module::getOrdered() as $module) {
-            $command->info("***** [" . $module->getName() . "] ****");
-            $command->call('module:seed', ['module' => $module->getName()]);
-        }/*
-          foreach ($this->modules as $module) {
-          if (\Module::has($module)) {
-          $command->call('module:seed', ['module' => $module]);
-          }
-          }
-          if ($command->option('seed')) {
-          $command->warn('Seeding extra data');
-          foreach ($this->secondary as $module) {
-          if (\Module::has($module)) {
-          $command->call('module:seed', ['module' => $module]);
-          }
-          }
-          } */
+//        foreach (\Module::getOrdered() as $module) {
+//            $command->info("***** [" . $module->getName() . "] ****");
+//            $command->call('module:seed', ['module' => $module->getName()]);
+//        }
+
+        foreach ($this->modules as $module) {
+            if (\Module::has($module)) {
+                $command->call('module:seed', ['module' => $module]);
+            }
+        }
+        if ($command->option('seed')) {
+            $command->warn('Seeding extra data');
+            foreach (\Module::getOrdered() as $module) {
+                if (\Module::has($module)) {
+                    $command->call('module:seed', ['module' => $module]);
+                }
+            }
+        } else {
+            $command->warn('Seeding few data');
+            foreach ($this->modules as $module) {
+                if (\Module::has($module)) {
+                    $command->call('module:seed', ['module' => $module]);
+                }
+            }
+        }
     }
 
 }
